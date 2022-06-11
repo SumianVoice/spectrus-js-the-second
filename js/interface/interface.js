587809d9922caf5ba20d5be60388d5da27bc986c
Sumyjkl <sumianvoice@gmail.com>
2022-06-12 01:00:18 +1000

    added comments, replaced crosshair with new ruler function which does the same thing

--- js/interface/interface.js
+++ js/interface/interface.js
@@ -1,8 +1,6 @@
 
 
-
-
-document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault()); // stop the rightclick menu from showing
 
 class _GUI {
   constructor(spec, container=window) {
@@ -53,56 +51,10 @@ class _GUI {
     this.ruler[0] = {x:x,y:y,active:true};
   }
   disableRuler() {
-    this.ruler.active = false;
-  }
-  drawCrosshair() {
-    if (this.mouse.y < 0 || this.mouse.y > this.viewPortBottom) {
-      return "out of bounds";
-    }
-
-    this.ctx.lineWidth = 1;
-    this.ctx.strokeStyle = "#ffffff30";
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.mouse.x, 0);
-    this.ctx.lineTo(this.mouse.x, this.canvas.height);
-    this.ctx.stroke();
-
-    this.ctx.lineWidth = 3;
-    this.ctx.strokeStyle = "#222";
-    this.ctx.beginPath();
-    this.ctx.moveTo(0, this.mouse.y);
-    this.ctx.lineTo(this.canvas.width, this.mouse.y);
-    this.ctx.stroke();
-
-    this.ctx.lineWidth = 1;
-    this.ctx.strokeStyle = "#fff";
-    this.ctx.beginPath();
-    this.ctx.moveTo(0, this.mouse.y);
-    this.ctx.lineTo(this.canvas.width, this.mouse.y);
-    this.ctx.stroke();
-
-      // Hz render
-    this.ctx.fillStyle = "#11111150"; // background for the reading
-    this.ctx.fillRect(this.mouse.x+1, this.mouse.y+1, 100, 70);
-    this.renderText(`${Math.floor(this.spec.hzFromY(this.mouse.y))}Hz`, this.mouse.x + 10, this.mouse.y + 20, "#ffffaa", "20px", "Mono");
-    this.renderText(`${Math.round(((this.viewPortRight-this.mouse.x)/this.spec.speed)*10)/10}s`, this.mouse.x + 10, this.mouse.y + 100, "#ffffaa", "20px", "Mono");
-
-      // note render
-    const tmpNote = lookupNote(this.spec.hzFromY(this.mouse.y)); // get the note at this position
-    if (tmpNote != "0") {
-      this.renderText(
-        `${tmpNote}`,
-        this.mouse.x + 10,
-        this.mouse.y + 40,
-        "#ffff5550", "20px", "Mono");
-      this.renderText(
-        `${Math.round(getNoteHz(tmpNote))}Hz`,
-        this.mouse.x + 10,
-        this.mouse.y + 60,
-        "#ffff5550", "20px", "Mono");
-    }
+    this.ruler[0].active = false;
   }
   drawRuler(x,y,color,width=1,fullWidth=true) {
+    // draw vert line
     this.ctx.lineWidth = width;
     this.ctx.strokeStyle = "#ffffff10";
     this.ctx.beginPath();
@@ -110,6 +62,7 @@ class _GUI {
     this.ctx.lineTo(x, this.canvas.height);
     this.ctx.stroke();
 
+    // draw horizontal background line
     this.ctx.lineWidth = width+2;
     this.ctx.strokeStyle = "#111";
     this.ctx.beginPath();
@@ -122,6 +75,7 @@ class _GUI {
     this.ctx.lineTo(this.canvas.width, y);
     this.ctx.stroke();
 
+    // draw horizontal line
     this.ctx.lineWidth = width;
     this.ctx.strokeStyle = color;
     this.ctx.beginPath();
@@ -135,20 +89,22 @@ class _GUI {
     this.ctx.stroke();
 
 
-      // Hz render
+    // Hz render
     this.ctx.fillStyle = "#11111150"; // background for the reading
     this.ctx.fillRect(x+1, y+1, 100, 70);
     this.renderText(`${Math.floor(this.spec.hzFromY(y))}Hz`, x + 10, y + 20, color, "20px", "Mono");
-    // this.renderText(`${Math.round(((this.viewPortRight-x)/this.spec.speed)*10)/10}s`, x + 10, y + 100, "#ffffaa", "20px", "Mono");
+    // this.renderText(`${Math.round(((this.viewPortRight-x)/this.spec.speed)*10)/10}s`, x + 10, y + 100, "#ffffaa", "20px", "Mono"); // show time, but it's broken?
 
-      // note render
+    // note render
     const tmpNote = lookupNote(this.spec.hzFromY(y)); // get the note at this position
     if (tmpNote != "0") {
+      // show the note
       this.renderText(
         `${tmpNote}`,
         x + 10,
         y + 40,
         color + "50", "20px", "Mono");
+      // show the hz of that note
       this.renderText(
         `${Math.round(getNoteHz(tmpNote))}Hz`,
         x + 10,
@@ -158,7 +114,7 @@ class _GUI {
   }
   update() {
     this.updateScale();
-    this.pitchAlert = parseInt(pitchFloorAlert.content);
+    this.pitchAlert = parseInt(pitchFloorAlert.content); // update the pitch alert
     if (this.mouse.keys.includes(0)) { // when press LMB
       this.drawRuler(this.mouse.x, this.mouse.y, "#ffff44", 2);
     }
