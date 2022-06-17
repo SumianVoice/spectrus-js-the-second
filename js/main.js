7ba5339137fac4d553a6821934f2a274c387ffbf
hle0 <91701075+hle0@users.noreply.github.com>
2022-06-17 16:29:34 -0400

    fix linting errors in main.js
    Signed-off-by: hle0 <91701075+hle0@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-// select an input, then start everything
-navigator.mediaDevices.getUserMedia({ audio: true }).then(initialise).catch(console.log);
-
-let audioSystem;
+let audioSystem; // eslint-disable-line no-unused-vars
 
 function initialise(stream) { // eslint-disable-line no-unused-vars
   audioSystem = new AudioSystem(document.querySelector('#div'), stream);
   startLoop(audioSystem.update.bind(audioSystem));
 }
 
+// select an input, then start everything
+// eslint-disable-next-line no-console
+navigator.mediaDevices.getUserMedia({ audio: true }).then(initialise).catch(console.error);
+
 //  add spacebar to pause
 const spaceBar = 32;
-window.onkeydown = function (gfg) {
+window.onkeydown = (gfg) => {
   if (gfg.keyCode === spaceBar) {
     audioSystem.spec.pauseToggle();
   }
